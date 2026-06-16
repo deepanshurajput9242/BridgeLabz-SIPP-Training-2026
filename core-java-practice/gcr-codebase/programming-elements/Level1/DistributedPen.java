086c32eae128c790b4c830f7f1dfbaa5b690ab62
Deepanshu Rajput <deepanshurajput9242.com>
2026-06-16 09:51:48 +0530

    [Deepanshu Rajput added to BridgeLabz]

--- core-java-practice/gcr-codebase/programming-elements/Level1/DistributedPen.java
+++
@@ -1,9 +0,0 @@
-public class DistributedPen {
-    public static void main(String[] args) {
-        int pens = 15;
-        int students = 5;
-        int pensperstudent = pens / students;
-        int rem = pens % students;
-        System.out.println("The Pen Per Student is " + pensperstudent + " and the remaining pen not distributed is " + rem);
-    }
-}
